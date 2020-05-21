9ab2f05d478ff0f0c4a85ca9210abb71ae1dfaad
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 18:43:06 +0530

    form completed

--- src/form.js
+++ src/form.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { FormControl, InputGroup, Button } from "react-bootstrap";
 
 const Form = () => {
+  const [todo, settodo] = useState("");
   return (
     <React.Fragment>
       <h3>#Todos for the day</h3>
+      <InputGroup className="mb">
+        <FormControl
+          required={true}
+          placeholder="Enter Todos...."
+          value={todo}
+          onChange={(e) => settodo(e.target.value)}
+        />
+      </InputGroup>
+      <Button
+        disabled={todo.length === 0 ? true : false}
+        onClick={() => {
+          addtodo(todo);
+        }}
+        variant="success"
+      >
+        Submit
+      </Button>
     </React.Fragment>
   );
 };
